2a6b76072557cf10a09027efb88c17ce0e37737b
Sergey Petrachev <88962946+wolk8396@users.noreply.github.com>
2021-12-16 07:54:13 +0300

    Update homework11.js

--- homework11.js
+++ homework11.js
@@ -1,107 +1,72 @@
-/// 1 exercise
+/* const array = [
+  { city:'London',   departureTime: '12:00 p.m'},
+  { city:'Moscow',   departureTime: '9:00 p.m'},
+  { city:'New York', departureTime: '10:00 p.m'}
+  ];
 
+function propertyValue (arr, key) {
+  const result = [];
 
-/* const arr = [1, 2, 3, 4, 5];
+  arr.forEach((item, i) => {
+      arr.length = key
+      result.push(item);
+    },);
 
-const  removeItem = arr => {
-  return arr.splice(0, 1);
-}
-console.log(removeItem(arr));
+    return  result;
+  }
+
+  console.log(propertyValue (array, 2));
  */
 
-/* const arrFirst = [1, 2, 3, 4, 5];
-const copy = [];
+/* const array = [1, 2, 3, 4, 5]
 
-const arrSecond = arrFirst.map((item, index) => {
-  if ( index === 0)
-    return copy.push(item);
-});
-console.log(copy); */
+const removeItem =(arr, num) => {
+  const result = [];
 
+  arr.filter((item ,index) => {
+   if ( index < num) {
+     result.push(item)
+   }
+  });
 
-
-
-////////// 2 exercise
-/* const arr = [1, 2, 3, 4, 5];
-
-const  removeItem = arr => {
-  return arr.splice(4, 3);
+  return  result;
 }
-console.log(removeItem(arr)); */
- 
 
-///// map
-/* const arrFirst = [1, 2, 3, 4, 7];
+console.log(removeItem(array, 4)); */
 
-const arrSecond = arrFirst.map(item => {
-  return arrFirst.splice(item[3],[4]);
-});
-console.log(arrFirst);
+
+
+/* const arr = ['home', 'light', 'hi', 'world'];
+
+function findLength(arr) {
+  const  copy = [];
+
+  arr.reduce((acc, item) => {
+    copy.push(item.length);
+
+    return acc + item.length;
+  },0);
+
+  return copy;
+}
+
+console.log( findLength(arr));
  */
 
-/* const arrFirst = [1, 2, 3, 4, 5];
-const copy = [];
+/* 
+const arr = [2, 3, 5, 7, 11, 13, 17, 19];
+function currentSums (arr) {
+const result = [];
 
-const arrSecond = arrFirst.map((item, index, arr) => {
-  if ( index === arr.length-1)
-    return copy.push(item);
-});
-console.log(copy);
- */
+const away = arr.reduce( (acc, item, i) => {
+  result.push(acc);
 
-///////////3 exercise
-/* const newBox = [];
+  return  acc + item;
+  },0);
+result.push(away);
 
-const arrayFilled = (value, len) => {
-  for (let i = 0; i < len; i++) {
-    newBox.push(value);
-  }
-  return newBox;
+return result;
 }
-console.log(arrayFilled(4, 5)); */
 
-
-/////////4 exercise
-/* const newBox = [];
-
-const generateRange = (start, len) => {
-  for (let i = start; i < len; i++) {
-    newBox.push(start);
-    start++
-  }
-  return newBox;
-}
-console.log(generateRange(4, 10)); */
-
-/////////////////////////////////////////////////////////////////////////////////////
-///5 exercise
-
- /*const array = 'Каждый охотник желает знать, где сидит фазан';
-const copy = [];
-let arr = array.split(' ');
-
-const  x = arr.forEach(item => {
-  copy.push(item[0])
-},);
-console.log(copy); */
-
-
-/* const array = 'Каждый охотник желает знать, где сидит фазан';
-const copy = [];
-let arr = array.split(' ');
-
-const findLetters = arr => {
-  for (let i = 0; i < arr.length; i++) {
-   copy.push(arr[i][0].split(' '));
-  }
-  return copy.join(' ');
-}
-console.log(findLetters(arr));  */
-
-
-
-
-
-
-
+console.log(currentSums (arr)); */
 
